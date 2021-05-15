e183e8d4270a80678394db4d77f86edb6b400cf7
mountaindew2141@outlook.com <mountaindew2141@outlook.com>
2021-05-15 13:03:23 -0400

    added more pokemon

--- seeds/pokemon-seeds.js
+++ seeds/pokemon-seeds.js
@@ -33,6 +33,134 @@ const pokemonData = [
     attack: "30",
     defense: "35"
   },
+  {
+    id: 5,
+    name: "Charizard",
+    type: "Fire",
+    hp: "78",
+    attack: "84",
+    defense: "78"
+  },
+  {
+    id: 6,
+    name: "Blastoise",
+    type: "Water",
+    hp: "79",
+    attack: "83",
+    defense: "100"
+  },
+  {
+    id: 7,
+    name: "Beedrill",
+    type: "Bug",
+    hp: "65",
+    attack: "90",
+    defense: "40"
+  },
+  {
+    id: 8,
+    name: "Pidgey",
+    type: "Normal",
+    hp: "40",
+    attack: "45",
+    defense: "40"
+  },
+  {
+    id: 9,
+    name: "Rattata",
+    type: "Normal",
+    hp: "30",
+    attack: "56",
+    defense: "35"
+  },
+  {
+    id: 10,
+    name: "Raichu",
+    type: "Electric",
+    hp: "60",
+    attack: "90",
+    defense: "55"
+  },
+  {
+    id: 11,
+    name: "Sandshrew",
+    type: "Ground",
+    hp: "50",
+    attack: "75",
+    defense: "85"
+  },
+  {
+    id: 12,
+    name: "Nidoran",
+    type: "Poison",
+    hp: "55",
+    attack: "47",
+    defense: "52"
+  },
+  {
+    id: 13,
+    name: "Clefairy",
+    type: "Fairy",
+    hp: "70",
+    attack: "45",
+    defense: "48"
+  },
+  {
+    id: 14,
+    name: "Vulpix",
+    type: "Fire",
+    hp: "38",
+    attack: "41",
+    defense: "40"
+  },
+  {
+    id: 15,
+    name: "Ninetails",
+    type: "Fire",
+    hp: "73",
+    attack: "76",
+    defense: "75"
+  },
+  {
+    id: 16,
+    name: "Jigglypuff",
+    type: "Fairy",
+    hp: "115",
+    attack: "45",
+    defense: "20"
+  },
+  {
+    id: 17,
+    name: "Zubat",
+    type: "Poison",
+    hp: "40",
+    attack: "45",
+    defense: "35"
+  },
+  {
+    id: 18,
+    name: "Paras",
+    type: "Bug",
+    hp: "35",
+    attack: "70",
+    defense: "55"
+  },
+  {
+    id: 19,
+    name: "Diglett",
+    type: "Ground",
+    hp: "10",
+    attack: "55",
+    defense: "25"
+  },
+  {
+    id: 20,
+    name: "Meowth",
+    type: "Normal",
+    hp: "40",
+    attack: "45",
+    defense: "35"
+  },
 ]
 
 const seedPokemon = () => Pokemon.bulkCreate(pokemonData);
