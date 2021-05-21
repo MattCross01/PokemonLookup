96af6698eadbccf926ac6d1777ad2766fbb5090a
mountaindew2141@outlook.com <mountaindew2141@outlook.com>
2021-05-20 21:54:15 -0400

    added profile.handlebars and homeRoutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,28 @@
+  
+const router = require('express').Router();
+const { User, Pokemon } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try{
+        const pokemonData = await Pokemon.findAll({
+            include: [
+                {
+                  model: User,
+                  attributes: ['name'],
+                },
+            ],
+        });
+
+        const pokemons = pokemonData.map((pokemon) => pokemon.get({ plain: true }));
+
+        res.render('homepage', {
+            pokemons,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
